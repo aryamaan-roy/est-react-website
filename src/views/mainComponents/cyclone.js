950e3fb1d137ab84ccdf6eeb62e54c63857059a9
Aryamaan Basu Roy <67855588+aryamaan-roy@users.noreply.github.com>
2022-11-18 11:59:27 +0530

    commit 2

--- src/views/mainComponents/cyclone.js
+++ src/views/mainComponents/cyclone.js
@@ -17,6 +17,10 @@ import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-route
 import { useEffect } from 'react'
 import { auth } from '../../firebase'
 import db from '../../firebase1'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 
 function Copyright(props) {
@@ -36,25 +40,42 @@ const theme = createTheme();
 
 export default function Cyclone_input() {
   const navigate = useNavigate()
+
+
+  const [type, setType] = React.useState('');
+
+  const handleChangeType = (event) => {
+    setType(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      type : type,
+      name: data.get('name'),
+      location: data.get('location'),
+      ocean: data.get('ocean'),
+      peak_speed: data.get('peak-speed'),
+      loss: data.get('loss'),
+      gdp_drop: data.get('gdp-drop'),
+      damage: data.get('damage'),
+      death: data.get('death'),
+      date_formed: data.get('date-formed'),
+      date_end: data.get('date-end'),
     });
-    auth
-      .signInWithEmailAndPassword(data.get('email'), data.get('password'))
-      .then((user) => {
-        console.log(user)
-        localStorage.setItem('is_log', 'true')
-        console.log(localStorage.getItem('is_log'))
-        navigate('/Dashboard')
-        console.log('Log in successful')
-      })
-      .catch(function (err) {
-        alert('Invalid Credentials')
-      })
+    // auth
+    //   .signInWithEmailAndPassword(data.get('email'), data.get('password'))
+    //   .then((user) => {
+    //     console.log(user)
+    //     localStorage.setItem('is_log', 'true')
+    //     console.log(localStorage.getItem('is_log'))
+    //     navigate('/Dashboard')
+    //     console.log('Log in successful')
+    //   })
+    //   .catch(function (err) {
+    //     alert('Invalid Credentials')
+    //   })
   };
 
   return (
@@ -67,7 +88,7 @@ export default function Cyclone_input() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://images.unsplash.com/photo-1617791160536-598cf32026fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80)',
+            backgroundImage: 'url(https://images.unsplash.com/photo-1454789476662-53eb23ba5907?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=452&q=80)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
@@ -89,53 +110,133 @@ export default function Cyclone_input() {
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
-              Sign in
+              Cyclone Data
             </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
               <TextField
                 margin="normal"
                 required
                 fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
+                id="name"
+                label="Cyclone Name"
+                type="text"
+                name="name"
                 autoFocus
               />
               <TextField
                 margin="normal"
                 required
                 fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
+                name="location"
+                label="Location"
+                type="text"
+                id="location"
+                autoComplete="current-location"
               />
-              <FormControlLabel
+              <TextField
+                margin="normal"
+                required
+                halfWidth
+                name="ocean"
+                label="Ocean"
+                type="text"
+                id="ocean"
+                autoComplete="current-ocean"
+              />
+              <TextField
+                margin="normal"
+                required
+                halfWidth
+                name="peak-speed"
+                label="Peak-speed (kmph)"
+                type="decimal"
+                id="peak-speed"
+                autoComplete="current-peak-speed"
+              />
+              <TextField
+                margin="normal"
+                required
+                halfWidth
+                name="loss"
+                label="Monetory loss ($M)"
+                type="decimal"
+                id="loss"
+                autoComplete="current-loss"
+              />
+              <TextField
+                margin="normal"
+                required
+                halfWidth
+                name="gdp-drop"
+                label="GDP Drop (%)"
+                type="decimal"
+                id="gdp-drop"
+                autoComplete="current-gdp-drop"
+              />
+              <TextField
+                margin="normal"
+                required
+                halfWidth
+                name="damage"
+                label="Land Damage (Ha)"
+                type="number"
+                id="damage"
+                autoComplete="current-damage"
+              />
+              <TextField
+                margin="normal"
+                required
+                halfWidth
+                name="death"
+                label="Human Death"
+                type="number"
+                id="death"
+                autoComplete="current-death"
+              />
+              <TextField
+                margin="normal"
+                required
+                halfWidth
+                name="date-formed"
+                label="Cyclone Date Formed"
+                type="text"
+                id="date-formed"
+                autoComplete="current-date-formed"
+              />
+              <TextField
+                margin="normal"
+                required
+                halfWidth
+                name="date-end"
+                label="Cyclone Date Dissapated"
+                type="text"
+                id="date-end"
+                autoComplete="current-date-end"
+              />
+              <InputLabel id="demo-simple-select-label">Select INDIA/WORLD</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={type}
+                label="type"
+                onChange={handleChangeType}
+              >
+                <MenuItem value={"INDIA"}>INDIA</MenuItem>
+                <MenuItem value={"WORLD"}>WORLD</MenuItem>
+              </Select>
+              {/* <FormControlLabel
                 control={<Checkbox value="remember" color="primary" />}
                 label="Remember me"
-              />
+              /> */}
               <Button
                 type="submit"
                 fullWidth
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
               >
-                Sign In
+                Add Cyclone
               </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link onClick={() => { navigate('/SignUp') }} variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
+
               <Copyright sx={{ mt: 5 }} />
             </Box>
           </Box>
